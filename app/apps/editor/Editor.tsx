dac77d10a84c05d9b7e2780a3da93e75c45d3397
JohnWicc69420 <tusmantariq9111@gmail.com>
2024-02-08 19:10:21 +0500

    fix: editor fixed

--- app/apps/editor/Editor.tsx
+++ app/apps/editor/Editor.tsx
@@ -1,29 +1,20 @@
-import { useRef, useState, useEffect } from "react";
-import ReactQuill from "react-quill";
+import { useState } from "react";
+import QuillEditor from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-function App() {
+const App = () => {
   const [value, setValue] = useState("");
-  const editorRef = useRef<ReactQuill>(null);
-  const isFirstChange = useRef(true);
 
   return (
-    <div style={{ width: "700px", height: "500px", color: "#CED2D8" }}>
-      <ReactQuill
-        ref={editorRef}
+    <div className=" h-[500px] w-[250px] md:h-[450px] md:w-[600px] xl:h-[650px] xl:w-[1000px]">
+      <QuillEditor
+        className="h-[400px] w-[250px] md:h-[400px] md:w-[600px] xl:h-[600px] xl:w-[1000px] mb-8"
         theme="snow"
         value={value}
-        style={{ width: "100%", height: "400px", color: "#CED2D8" }}
-        onChange={(content) => {
-          if (isFirstChange.current) {
-            isFirstChange.current = false;
-            return;
-          }
-          setValue(content);
-        }}
+        onChange={(value) => setValue(value)}
       />
     </div>
   );
-}
+};
 
 export default App;
